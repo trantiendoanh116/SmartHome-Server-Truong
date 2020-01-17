f7a4e9348c9c2b244d6ccf2e3f4dacf71645b874
Truong <tientruong@coloralpha.com>
2020-01-17 22:12:08 +0700

    clear code

--- index.js
+++ index.js
@@ -153,14 +153,14 @@ esp8266_nsp.on('connection', function (socket) {
     console.log("Disconnect socket esp8266")
   });
 
-  var interval1 = setInterval(function() {
+  // var interval1 = setInterval(function() {
 	
-    //Cài đặt chuỗi JSON, tên biến JSON này là json 
-    var json = {};
-    json["F1_D01"] = "change";
-		esp8266_nsp.emit("CONTROL", json) //Gửi lệnh LED với các tham số của của chuỗi JSON
-		console.log("Change LED")//Ghi ra console.log là đã gửi lệnh LED
-	}, 4000)//1000ms
+  //   //Cài đặt chuỗi JSON, tên biến JSON này là json 
+  //   var json = {};
+  //   json["F1_D01"] = "change";
+	// 	esp8266_nsp.emit("CONTROL", json)
+	// 	console.log("Change LED")
+	// }, 4000)//1000ms
 
 
   socket.on("*", function (packet) {
