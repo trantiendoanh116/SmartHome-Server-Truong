8225c8d1227397eb3439b9ab3b535f331f9559a2
Tien Doanh <trantiendoanh116@gmail.com>
2020-02-25 22:53:53 +0700

    Control from web

--- public/js/home.js
+++ public/js/home.js
@@ -20,20 +20,26 @@ angular.module('myApp', [
 	const EVENT_RECEIVE_DATA = "DATA";
 	const EVENT_CONTROL = "CONTROL";
 	//Dùng để đặt các giá trị mặc định
-	$scope.F1_D01 = 0;
-	$scope.F1_D02 = 0;
+	$scope.F1_D06 = 0;
+	$scope.F1_D07 = 0;
+	$scope.F1_D08 = 0;
 	$scope.temp = 0;
 	$scope.humi = 0;
 	$scope.co = 0;
 
 	$scope.changeDenTran = function () {
 		var json = {};
-		json["F1_D01"] = "change";
+		json["F1_D06"] = "change";
 		mySocket.emit(EVENT_CONTROL, json);
 	};
 	$scope.changeDenChum = function () {
 		var json = {};
-		json["F1_D02"] = "change";
+		json["F1_D07"] = "change";
+		mySocket.emit(EVENT_CONTROL, json);
+	};
+	$scope.changeDenTranh = function () {
+		var json = {};
+		json["F1_D08"] = "change";
 		mySocket.emit(EVENT_CONTROL, json);
 	};
 
@@ -41,27 +47,30 @@ angular.module('myApp', [
 	mySocket.on(EVENT_RECEIVE_DATA, function (json) {
 		console.log(json)
 		
-		if (json.hasOwnProperty('F1_D01')) {
-			var value = json['F1_D01'];
-			$scope.F1_D01 = value
+		if (json.hasOwnProperty('F1_D06')) {
+			var value = json['F1_D06'];
+			$scope.F1_D06 = value
 		}
-		if (json.hasOwnProperty('F1_D02')) {
-			var value = json['F1_D02'];
-			$scope.F1_D02 = value
+		if (json.hasOwnProperty('F1_D07')) {
+			var value = json['F1_D07'];
+			$scope.F1_D07 = value
+		}
+		if (json.hasOwnProperty('F1_D08')) {
+			var value = json['F1_D08'];
+			$scope.F1_D08 = value
 		}
 	
-		if (json.hasOwnProperty('temp')) {
-			var value = json['temp'];
-			$scope.temp = value
+		if (json.hasOwnProperty('C_S01')) {
+			var temp = json['C_S01']['TEMP'] || 0
+			var humi = json['C_S01']['HUMI'] || 0
+			$scope.temp = temp
+			$scope.humi = humi
 		}
-		if (json.hasOwnProperty('humi')) {
-			var value = json['humi'];
-			$scope.humi = value
+		if (json.hasOwnProperty('C_S02')) {
+			$scope.co = json['C_S02']
+
 		}
-		if (json.hasOwnProperty('co')) {
-			var value = json['co'];
-			$scope.co = value
-		}
+		
 
 	});
 
